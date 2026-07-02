ea9c3e6b51fc4ee5f675d4f1a1a26f65495151e3
jh5117-debug <jh5117@columbia.edu>
2026-07-02 12:08:30 +0200

    Fix Exp60B H20 downloader python entrypoint

--- scripts/exp60b_h20_select_vpdata_subset.sh
+++ scripts/exp60b_h20_select_vpdata_subset.sh
@@ -5,10 +5,11 @@ REPO_ROOT="${REPO_ROOT:-/home/nvme01/H20_Video_inpainting_DPO_exp60b_vp_vpdata_t
 OUTPUT_ROOT="${OUTPUT_ROOT:-/home/nvme01/H20_Video_inpainting_DPO/data/external/vpdata_exp60b_h20_staging/raw_subset}"
 REPORTS_DIR="${REPORTS_DIR:-/home/nvme01/H20_Video_inpainting_DPO/reports/exp60b_vpdata_h20_download}"
 MANIFEST_DIR="${MANIFEST_DIR:-${REPO_ROOT}/manifests}"
+PYTHON_BIN="${PYTHON_BIN:-python3}"
 
 cd "${REPO_ROOT}"
 
-python -m exp60b_videopainter_vpdata_d3mask.vpdata_subset \
+"${PYTHON_BIN}" -m exp60b_videopainter_vpdata_d3mask.vpdata_subset \
   --output_root "${OUTPUT_ROOT}" \
   --manifest_dir "${MANIFEST_DIR}" \
   --reports_dir "${REPORTS_DIR}" \
@@ -17,4 +18,3 @@ python -m exp60b_videopainter_vpdata_d3mask.vpdata_subset \
   --max_test 100 \
   --source_filter pexels_only \
   "$@"
-
